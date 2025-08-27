1ad0fac2317257348043a5409b80c1414c5db2cd
bel947 <anabel.hg.97@gmail.com>
2025-08-27 14:31:10 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 27 13:09:12 2025
+
+@author: anabel.hernandez
+"""
+
+import streamlit as st
+import pandas as pd
+
+# Cargar los datos
+df = pd.read_csv(r'C:\Anabel\ventas.csv')
+
+# Mostrar en la web
+st.title("Mi app de ventas")
+st.write(df)
